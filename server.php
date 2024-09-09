03aaaa47cf09dc1f1cd5ec8a0b53a1bf658a014f
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-09-09 17:30:36 +0200

    [UPD] exported and encoded file to the script.js

--- server.php
+++ server.php
@@ -4,5 +4,6 @@ header('content_type: application/json');
 $todoListToFix = file_get_contents('./data/todo-list.json');
 $todoList = json_decode($todoListToFix, true);
 
+echo json_encode($todoList) ;
 
 ?>
