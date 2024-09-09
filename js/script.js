f48957a948ac17c3d12deaa382979e5aa0743aa8
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-09-09 16:05:25 +0200

    [UPD] defined the vue function to make the axios call

--- js/script.js
+++ js/script.js
@@ -3,11 +3,17 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
+            todoList: [],
+            url: 'server.php'
 
         }
     }, 
     methods: {
-        
+        getTodoList(){
+            axios.get(this.url).then((resp) => {
+                this.todolist = resp.data;
+            });
+        }
     },
     mounted() {
         
